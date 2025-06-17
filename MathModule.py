ba48c533a4e165c68738ec8aa32e82d170cdab49
Ayan Banerjee <ayanban4000@gmail.com>
2025-06-17 15:03:16 +0530

    Add files via upload

---
+++ MathModule.py
@@ -0,0 +1,10 @@
+import math
+num = float(input("Enter a number: "))
+
+sqrt_result = math.sqrt(num)
+log_result = math.log(num)
+sine_result = math.sin(num)
+
+print(f"Square root of {num} is: {sqrt_result}")
+print(f"Natural logarithm of {num} is: {log_result}")
+print(f"Sine of {num} (in radians) is: {sine_result}")
